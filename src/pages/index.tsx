1586a3f2d7dcc9781dca598d33f3529f2ebdb44e
Ke1too <s1f102301730@iniad.org>
2023-06-16 06:50:00 +0900

    初回マインスイーパー

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,49 +17,46 @@ const Home = () => {
     [1, 0],
     [1, -1],
   ];
-  const [userInputs,setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-    ]);
-    const bombCount = 10;
-    // 0 -> ボム無し 
-    // 1 -> ボム有り
-    const [bombMap, setbombMap] = useState([
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-    ]);
-    const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
-    const isFailure = userInputs.some((row,y) => 
-      row.some((input,x) => input === 1 && bombMap[y][x] === 1)
-    );
-    // -1 -> 石
-    // 0 -> 画像なしセル
-    // 1~8 -> 数字セル
-    // 9 -> 石＋はてな
-    // 10 -> 石＋旗
-    // 11 -> ボムセル
-    const board: number[][] = [];
-      
-    const onClick = (x: number, y: number) => {
-      console.log(x, y);
-      const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-  return (
-    <div className={styles.container}>
-      
-    </div>
+  const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const bombCount = 10;
+  // 0 -> ボム無し
+  // 1 -> ボム有り
+  const [bombMap, setbombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+  const isFailure = userInputs.some((row, y) =>
+    row.some((input, x) => input === 1 && bombMap[y][x] === 1)
   );
+  // -1 -> 石
+  // 0 -> 画像なしセル
+  // 1~8 -> 数字セル
+  // 9 -> 石＋はてな
+  // 10 -> 石＋旗
+  // 11 -> ボムセル
+  const board: number[][] = [];
+
+  const onClick = (x: number, y: number) => {
+    console.log(x, y);
+    const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+    return <div className={styles.container} />;
+  };
 };
 
 export default Home;
