bd44f53dd62f0dfed5550ed5a2e96f63914596d7
Ke1too <s1f102301730@iniad.org>
2023-06-18 08:52:24 +0900

    マインスイーパー2日目

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,6 @@ const Home = () => {
   // 2 -> はてな
   // 3 -> 旗(右クリック)
 
-  const directions = [
-    [-1, -1],
-    [-1, 0],
-    [-1, 1],
-    [0, 1],
-    [0, -1],
-    [1, 1],
-    [1, 0],
-    [1, -1],
-  ];
   const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -27,7 +17,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const bombCount = 10;
+
   // 0 -> ボム無し
   // 1 -> ボム有り
   const [bombMap, setbombMap] = useState([
@@ -50,13 +40,60 @@ const Home = () => {
   // 9 -> 石＋はてな
   // 10 -> 石＋旗
   // 11 -> ボムセル
-  const board: number[][] = [];
+  const board: number[][] = [
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+  ];
 
-  const onClick = (x: number, y: number) => {
+  const directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+  ];
+  const bombCount = 10;
+
+  const clickLeft = (x: number, y: number) => {
     console.log(x, y);
-    const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    return <div className={styles.container} />;
+    const newbombMap: number[][] = JSON.parse(JSON.stringify(board));
+
+    let bomb = 0;
+    while (bomb < 10) {
+      const s = Math.floor(Math.random() * 9);
+      const t = Math.floor(Math.random() * 9);
+
+      if (!newbombMap[s][t]) {
+        console.log(bomb);
+        newbombMap[s][t] = 1;
+        bomb += 1;
+      }
+    }
+
+    setbombMap(newbombMap);
   };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickLeft(x, y)} />
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
